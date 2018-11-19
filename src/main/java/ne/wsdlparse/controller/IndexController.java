123b5ca8eafdd7a9813b123d65249cd90ac90413
nour-elghareeb <nour.algharip@gmail.com>
2018-11-19 13:47:44 +0200

    Added 1 operation: DeleteWSDL Added ESQL Generation design (under dev still)

--- src/main/java/ne/wsdlparse/controller/IndexController.java
+++ src/main/java/ne/wsdlparse/controller/IndexController.java
@@ -5,14 +5,21 @@
  */
 package ne.wsdlparse.controller;
 
+import com.sun.xml.internal.ws.util.StringUtils;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import ne.utils.MiscUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.RequestParam;
+import wsdlparse.ne.DeleteWSDLRequest;
+import wsdlparse.ne.DeleteWSDLResponse;
+import wsdlparse.ne.GenerateMessageESQLRequest;
+import wsdlparse.ne.GenerateMessageESQLResponse;
 import wsdlparse.ne.ListAvailableWSDLsRequest;
 import wsdlparse.ne.ListAvailableWSDLsResponse;
 import wsdlparse.ne.ListOperationMessagesRequest;
@@ -150,12 +157,61 @@ public class IndexController {
         } catch (WSDLParserFault ex) {
             Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
             model.addAttribute("errorMessage", ex.getFaultInfo().getErrorMessage());
+            model.addAttribute("error", ex.getFaultInfo().getErrorMessage());
             model.addAttribute("exception", ex);
             return "error";
         }
     }
+    @RequestMapping(method = GET, value = "/View/{wsdlName}/{serviceName}/{portName}/{operationName}/{messageType}/{messageName}")
+    public String generateESQL(@PathVariable String wsdlName, @PathVariable String serviceName, @PathVariable String portName, @PathVariable String operationName, @PathVariable(required = false) String messageType, @PathVariable(required = false) String messageName, Model model) {
+        model.addAttribute("page", "view");
+        try {
+            String crumb = "";
+            GenerateMessageESQLRequest request = new GenerateMessageESQLRequest();
+            request.setWSDLName(wsdlName);
+            request.setServiceName(serviceName);
+            request.setPortName(portName);
+            request.setOperationName(operationName);
+            request.setMessageName(messageName == null ? messageType : messageName);
+            request.setESQLSource("output");
+            request.getESQLVerboisty().add("VALUE_HELP");
+            request.setUseReferenceAsVariables(true);
+            model.addAttribute("crumbs", new String[]{"WSDL#" + wsdlName, "Service#" + serviceName, "Port#" + portName, "Operation#" + operationName, messageType+"#"+messageName});
+            model.addAttribute("wsdlName", wsdlName);
+            model.addAttribute("serviceName", serviceName);
+            model.addAttribute("portName", portName);
+            model.addAttribute("messageType", StringUtils.capitalize(messageType));
+            model.addAttribute("messageName", messageName);
+            model.addAttribute("currentAction", "listMessages");
+            
+            model.addAttribute("currentPath", "/View/" + wsdlName + "/" + serviceName + "/" + portName + "/" + operationName);
+            GenerateMessageESQLResponse response = new WSDLParser_Service().getWSDLParserSOAP().generateMessageESQL(request);
+            model.addAttribute("items", response.getESQLLine());            
 
+        } catch (WSDLParserFault ex) {
+            Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
+            model.addAttribute("errorMessage", ex.getFaultInfo().getErrorMessage());
+            model.addAttribute("error", ex.getFaultInfo().getErrorMessage());
+            model.addAttribute("exception", ex);
+            
+        }
+        return "generate";
+    }
     
+    @PostMapping("/delete")
+    public String delete(@RequestParam(value="wsdlName", required = true) String wsdlName, Model model){
+        try {
+            DeleteWSDLRequest request = new DeleteWSDLRequest();
+            request.setWSDLName(wsdlName);
+            DeleteWSDLResponse response = new WSDLParser_Service().getWSDLParserSOAP().deleteWSDL(request);
+            model.addAttribute("success", "WSDL deleted successfully!");
+            return list(model);
+        } catch (WSDLParserFault ex) {
+            Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
+            model.addAttribute("error", ex.getFaultInfo().getErrorMessage());
+            return list(model);
+        }
+    }
 
     @RequestMapping(method = GET, value = "/test")
     public String test(Model model) {
